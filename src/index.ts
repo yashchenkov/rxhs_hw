aa906210f2880aaab7aaeb8bb0fccdc5b9b17fe3
Yashchenko Victor <Andrey@Komp11.local>
2022-12-08 20:52:49 +0300

    ?

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 import { ajax } from 'rxjs/ajax';
+import { forkJoin } from 'rxjs';
+
+forkJoin(
+    {
+        google: ajax.getJSON('https://api.github.com/users/google'),
+        microsoft: ajax.getJSON('https://api.github.com/users/microsoft'),
+        users: ajax.getJSON('https://api.github.com/users')
+    }
+)
+    .subscribe(console.log);
+
 
 const data$ = ajax.getJSON('https://api.github.com/search/repositories?q=rxjs');
 
@@ -7,3 +18,5 @@ data$.subscribe((value) => console.log('data$ value', value));
 const dataGitLab$ = ajax.getJSON('https://gitlab.com/api/v4/projects?search=nodejs');
 
 dataGitLab$.subscribe((value) => console.log('dataGitLab$ value', value));
+
+
